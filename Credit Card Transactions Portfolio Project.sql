6323aabb5a7fea0a0d3dae6434079d5b0d97db0d
Gururaj Teli <57586509+GururajTeli@users.noreply.github.com>
2023-12-02 10:05:48 +0530

    Update Credit Card Transactions Portfolio Project.sql

--- Credit Card Transactions Portfolio Project.sql
+++ Credit Card Transactions Portfolio Project.sql
@@ -1,6 +1,17 @@
 /* SQL Portfolio project - Credit Card Transactions
 import the credit_card_transcations.xls file in any sql platform with table name : credit_card_transcations
-write 4-6 queries to explore the dataset and solve below questions */
+write 4-6 queries to explore the dataset and solve below questions 
+
+1- write a query to print top 5 cities with highest spends and their percentage contribution of total credit card spends 
+2- write a query to print highest spend month and amount spent in that month for each card type
+3- write a query to print the transaction details(all columns from the table) for each card type when
+it reaches a cumulative of 1000000 total spends(We should have 4 rows in the o/p one for each card type)
+4- write a query to find city which had lowest percentage spend for gold card type
+5- write a query to print 3 columns:  city, highest_expense_type , lowest_expense_type (example format : Delhi , bills, Fuel)
+6- write a query to find percentage contribution of spends by females for each expense type
+7- which card and expense type combination saw highest month over month growth in Jan-2014
+8- during weekends which city has highest total spend to total no of transcations ratio 
+9- which city took least number of days to reach its 500th transaction after the first transaction in that city. */
 
 /* 1- write a query to print top 5 cities with highest spends and their percentage contribution 
 of total credit card spends */
